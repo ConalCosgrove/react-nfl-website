c96893b67497a122b97b70c3bde6532ef21b5aa9
Conal Patrick Cosgrove <cosgroco@tcd.ie>
2018-04-10 00:20:04 +0100

    slightly refactored App.js

--- src/App.js
+++ src/App.js
@@ -15,14 +15,13 @@ class App extends Component {
       gameHTML : null
     }
 
-
     axios.get('https://nfl-app-backend.herokuapp.com/scoreJSON')
     .then(response => {
       this.state.games = response.data;
       this.getMatches();
 
     }).catch((err)=> {
-      this.setState({gameHTML: <span className="Loading"> Error: failed to load match data 😞🏈 </span>})
+      this.setState({gameHTML: <span className="Loading"> Error: failed to load match data</span>})
     });
   }
 
@@ -32,23 +31,20 @@ class App extends Component {
   }
 
   getMatches(){
-
     var arr = [];
-   
-
     var gameKeys = Object.keys(this.state.games);
 
     for (var key in gameKeys){
-      if(gameKeys[key] === 'nextupdate'){
-
-      }else{
+      if(gameKeys[key] !== 'nextupdate'){
         arr.push(this.state.games[gameKeys[key]]);
       }
     }
-
-   
-      this.setState({gameHTML: <div className = "Matches-Holder">{arr.map(item => <Match key={item.away.abbr} team1={item.home.abbr + ".svg"} team2={item.away.abbr + ".svg"} team1score={item.home.score.T} team2score={item.away.score.T} clock={item.clock} quarter={item.qtr}/>)}</div>})
-      
+    this.setState(	{gameHTML: 	<div className = "Matches-Holder"> 
+    								{arr.map(item => 
+    									<Match key={item.away.abbr} team1={item.home.abbr + ".svg"} team2={item.away.abbr + ".svg"} team1score={item.home.score.T} team2score={item.away.score.T} clock={item.clock} quarter={item.qtr}/>
+    								)}
+    							</div>
+    				}) 
     }
 
   render() {
@@ -56,23 +52,19 @@ class App extends Component {
 
     return (
       
-      <div className="App">
+    	<div className="App">
 
-        <header className="App-header">
-          <img src={PHI} className="Top-logo" alt="logo" onClick={this.refreshClick} />
-         
-        </header>
- 
-
+        	<header className="App-header">
+          		<img src={PHI} className="Top-logo" alt="logo" onClick={this.refreshClick} />
+        	</header>
           
-          {(this.state.gameHTML != null) ? this.state.gameHTML : <span className="Loading"> Loading Games...<small className = "Smol"> (If you see this screen for a long time, try running unsafe scripts in right of your url bar) </small> </span>}
+        	{(this.state.gameHTML != null) ? this.state.gameHTML : <span className="Loading"> Loading Games...<small className = "Smol"> (If you see this screen for a long time, try running unsafe scripts in right of your url bar) </small> </span>}
 
-          
-	      <div className = "TagLine">
-	      	<h3>All Times In GMT. Made with React {"<"}3 </h3>
-	        <img src = {logo} className = "App-logo" alt = "logo"/> 
-	      </div>
-      </div>
+	    	<div className = "TagLine">
+	      		<h3>All Times In GMT. Made with React {"<"}3 </h3>
+	        	<img src = {logo} className = "App-logo" alt = "logo"/> 
+	    	</div>
+      	</div>
     );
   }
 }
